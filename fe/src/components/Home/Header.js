e55d9019b72e36ed66b0293130610f7103ff3b61
Davin Iddo Irawan Alfian <daviniddo26@gmail.com>
2022-05-26 23:42:30 +1000

    Change image from HTML element to CSS only

--- fe/src/components/Home/Header.js
+++ fe/src/components/Home/Header.js
@@ -11,9 +11,7 @@ function Header() {
                     or Read the ones that has been weaved by others!
                 </p>
             </div>
-            <img className='header-image' alt=''
-                src='https://images.pexels.com/photos/278918/pexels-photo-278918.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'>    
-            </img>
+            <div className='header-image' />
         </div>
     );
 }
