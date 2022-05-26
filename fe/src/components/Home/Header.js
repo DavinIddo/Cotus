2f0ec988c92e833cc51fa9ae8ac2c33d6fe233ac
Davin Iddo Irawan Alfian <daviniddo26@gmail.com>
2022-05-26 13:00:38 +1000

    Modified the header

--- fe/src/components/Home/Header.js
+++ fe/src/components/Home/Header.js
@@ -12,7 +12,7 @@ function Header() {
                 </p>
             </div>
             <img className='header-image' alt=''
-                src='https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'>    
+                src='https://images.pexels.com/photos/278918/pexels-photo-278918.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'>    
             </img>
         </div>
     );
