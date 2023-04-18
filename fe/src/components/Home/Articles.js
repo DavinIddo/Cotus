ff30aae810a312b0d60f181d4eae38a83d845cce
Davin Iddo Irawan Alfian <daviniddo26@gmail.com>
2023-04-18 16:52:34 +1000

    Allow user to access a specific post content with details

--- fe/src/components/Home/Articles.js
+++ fe/src/components/Home/Articles.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Article from './Article';
 import './Articles.css'
 
@@ -10,6 +11,7 @@ function Articles() {
         fetch("/api/article/fetchAllArticles")
             .then(response => response.json())
             .then(data => {
+                console.log(data)
                 setContent(data["articles"])
             })
             .catch(error => {
@@ -20,7 +22,9 @@ function Articles() {
     return (
         <div className='articles'>
             {content.map((article, index) => (
-                <Article key={index} title={article.title} description={article.description} createdAt={article.createdAt} />
+                <Link className='article-link' to={'/single/' + article._id} key={index} state={article}>
+                    <Article title={article.title} description={article.description} createdAt={article.createdAt} />
+                </Link>
             ))}
         </div>
     );
--- fe/src/components/SinglePost/SinglePost.js
+++ fe/src/components/SinglePost/SinglePost.js
@@ -1,49 +1,31 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import "./SinglePost.css"
 
 function SinglePost() {
+    // Allow us to retrieve the data needed in the "State" parameter within Link component //
+    const location = useLocation();
+    const postDetail = location.state;
+
     return (
         <div className='singlepost'>
             <div className='singlepost-wrapper'>
-                <img className='singlepost-image' alt='' 
+                <img className='singlepost-image' alt=''
                     src='https://images.pexels.com/photos/300857/pexels-photo-300857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'>
                 </img>
                 <h1 className='singlepost-title'>
-                    The mysterious white sea
+                    {postDetail.title}
                     <div className='singlepost-modification'>
                         <i className="singlepost-icon fa-solid fa-pen-to-square edit"></i>
                         <i className="singlepost-icon fa-solid fa-trash delete"></i>
                     </div>
                 </h1>
                 <div className='singlepost-info'>
-                    <span className='singlepost-author'>Author: <b>Gero</b> </span>
-                    <span className='singlepost-date'>4 January 2020</span>
+                    <span className='singlepost-author'>Author: <b>{postDetail.author}</b> </span>
+                    <span className='singlepost-date'>{postDetail.createdAt}</span>
                 </div>
                 <p className='singlepost-description'>
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore. 
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore.
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore.
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore.
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore.
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore.
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore.
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore.
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore.
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore.
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore.
-                    Qui in consectetur magna qui ad laborum nostrud consectetur consectetur aliqua sunt. 
-                    Sint ut commodo quis nulla eiusmod tempor ullamco dolore voluptate sunt dolor. Nulla nulla do cupidatat pariatur ea adipisicing nisi qui dolore.
+                    {postDetail.description}
                 </p>
             </div>
         </div>
